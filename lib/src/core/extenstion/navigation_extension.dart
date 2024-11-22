1180e1402a77e6a5cf63bb6898fb3ad0b92452fd
ho3eintahan <hoseintahan84@gmail.com>
2024-11-22 16:55:48 +0330

    Update   navigation_extension.dart

--- lib/src/core/extenstion/navigation_extension.dart
+++ lib/src/core/extenstion/navigation_extension.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 extension NavigationExtension on BuildContext{
   
-  navigate(Widget widget)=> Navigator.of(this).push(MaterialPageRoute(builder: (context) => widget,));
+  navigate(Widget widget)=> Navigator.of(this).push(MaterialPageRoute(builder: (context) => widget));
   
   navigateReplacement(Widget widget)=> Navigator.of(this).pushReplacement(MaterialPageRoute(builder: (context) => widget));
   
