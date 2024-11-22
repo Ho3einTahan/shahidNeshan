f3aa85ea690198823e20c7af4c36ccbecd8198bb
ho3eintahan <hoseintahan84@gmail.com>
2024-11-22 16:56:13 +0330

    Update   bloc_providers.dart

--- lib/src/core/utils/bloc_providers.dart
+++ lib/src/core/utils/bloc_providers.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-MultiBlocProvider blocProviders (Widget child)=> MultiBlocProvider(
-  providers: [], 
-  child: child);
+MultiBlocProvider blocProviders(Widget child) => MultiBlocProvider(providers: [], child: child);
