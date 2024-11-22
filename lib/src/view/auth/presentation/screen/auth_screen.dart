6ebf1b6a9419a90eb3001d4a4f71840bdd06d86f
ho3eintahan <hoseintahan84@gmail.com>
2024-11-22 17:38:49 +0330

    Add   auth_screen.dart

---
+++ lib/src/view/auth/presentation/screen/auth_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => AuthScreenState();
+}
+
+class AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column()
+    );
+  }
+}
