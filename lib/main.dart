18ce6d12a4c2995bfbd298ad653aeb781b587a7d
ho3eintahan <hoseintahan84@gmail.com>
2024-11-23 19:37:01 +0330

    Save changes before merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
-
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shahid_neshan/src/config/get_it.dart';
 import 'package:shahid_neshan/src/config/theme.dart';
+import 'package:shahid_neshan/src/view/auth/presentation/screen/login_register_screen.dart';
+import 'package:shahid_neshan/src/view/auth/presentation/screen/verify_otp_screen.dart';
+import 'package:shahid_neshan/src/view/home/story-screen.dart';
+import 'package:shahid_neshan/src/view/location/presentaion/screen/location_screen.dart';
 import 'package:shahid_neshan/src/view/location/presentaion/screen/onboarding_screen.dart';
 
-void main() async{
-
-  runApp(Application());
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  GetItDP.setUpGetIt();
+  runApp(const Application());
 }
-
 
 class Application extends StatelessWidget {
   const Application({super.key});
@@ -18,8 +19,9 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: CustomeTheme.theme,
-      home: OnboardingScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: CustomTheme.theme,
+      home: const StoryScreen(),
     );
   }
 }
