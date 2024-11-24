5a38fbe0983f555a799499c559cb332764297b37
ho3eintahan <hoseintahan84@gmail.com>
2024-11-24 13:12:31 +0330

    Update    location_screen.dart

--- lib/src/view/location/presentaion/screen/location_screen.dart
+++ lib/src/view/location/presentaion/screen/location_screen.dart
@@ -19,38 +19,66 @@ class _LocationScreenState extends State<LocationScreen> {
   void initState() {
     super.initState();
     markers = [
-      // Marker(point: const LatLng(33.98308 ,51.43644), child: markerWidget())
+      Marker(
+        point: const LatLng(33.98308, 51.43644),
+        child: markerWidget(MartyrModel('hossein ', "")),
+      ),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(options: const MapOptions(initialCenter: LatLng(33.98308, 51.43644), initialZoom: 13), children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          maxNativeZoom: 12,
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(33.98308, 51.43644),
+          initialZoom: 13,
         ),
-        MarkerLayer(markers: markers)
-      ]),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            maxNativeZoom: 12,
+          ),
+          MarkerLayer(
+            markers: markers,
+          ),
+        ],
+      ),
     );
   }
 
   Widget markerWidget(MartyrModel data) => Container(
-        alignment: Alignment.center,
-        child: Stack(
-          children: [
-            SvgPicture.asset("assets/icon/marker.svg", fit: BoxFit.cover, width: 70, height: 70),
-            Container(
-                width: 40,
-                height: 40,
-                margin: const EdgeInsets.only(left: 5, top: 5),
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  boxShadow: [BoxShadow(blurRadius: 5, color: Colors.black)],
-                ),
-                child: ClipRRect(borderRadius: BorderRadius.circular(100), child: Image.network("https://old.aviny.com/album/tasvir-sazi/shakhes/kamel/40.jpg", fit: BoxFit.cover)))
-          ],
+    alignment: Alignment.center,
+    child: Stack(
+      children: [
+        SvgPicture.asset(
+          "assets/icon/marker.svg",
+          fit: BoxFit.cover,
+          width: 70,
+          height: 70,
         ),
-      );
+        Container(
+          width: 50,
+          height: 50,
+          margin: const EdgeInsets.only(left: 5, top: 5),
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 5,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.network(
+              "https://old.aviny.com/album/tasvir-sazi/shakhes/kamel/40.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
