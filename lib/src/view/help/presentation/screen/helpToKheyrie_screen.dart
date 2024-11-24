18678f259235f441670a9c54abb7f07ff0f438fe
ho3eintahan <hoseintahan84@gmail.com>
2024-11-24 17:20:34 +0330

    Add   helpToKheyrie_screen.dart

---
+++ lib/src/view/help/presentation/screen/helpToKheyrie_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HelpToKheyrieScreen extends StatelessWidget {
+  const HelpToKheyrieScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
