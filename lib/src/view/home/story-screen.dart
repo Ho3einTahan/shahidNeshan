72fc84bd6dcc2e740c3357142128f7fc00671d48
ho3eintahan <hoseintahan84@gmail.com>
2024-11-24 12:48:35 +0330

    Update   story-screen.dart

--- lib/src/view/home/story-screen.dart
+++ lib/src/view/home/story-screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:story/story_page_view.dart';
 
@@ -8,14 +10,17 @@ class StoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: StoryPageView(
+        indicatorVisitedColor: Colors.grey,
         itemBuilder: (context, pageIndex, storyIndex) {
-          return Center(
-            child: Text("Index of PageView: $pageIndex Index of story on each page: $storyIndex"),
+          return Stack(
+            children: <Widget>[
+              Positioned.fill(child: Image.asset("images/shahid-motahari.png", fit: BoxFit.cover)),
+              Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12), child: Container(color: Colors.black.withOpacity(0.2)))),
+              Positioned.fill(child: Image.asset("images/shahid-motahari.png")),
+            ],
           );
         },
-        storyLength: (pageIndex) {
-          return 3;
-        },
+        storyLength: (pageIndex) => 3,
         pageLength: 4,
       ),
     );
