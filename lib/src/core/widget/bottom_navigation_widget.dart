3a21013704b003dc90b83e11840433acca55c5b5
ho3eintahan <hoseintahan84@gmail.com>
2024-11-25 23:15:54 +0330

    Update   bottom_navigation_widget.dart

--- lib/src/core/widget/bottom_navigation_widget.dart
+++ lib/src/core/widget/bottom_navigation_widget.dart
@@ -23,67 +23,51 @@ class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        body: IndexedStack(index: _selected, children: screens),
+        body: IndexedStack(index: _selected, children: screens,),
         bottomNavigationBar: Container(
-          decoration: const BoxDecoration(border: Border(top: BorderSide(color: Color(0xffE6E6E6), width: 1))),
-          height: 65,
+          decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Color(0xffE6E6E6),width: 1),
+            )
+          ),
+          height: 62,
           child: BottomNavigationBar(
             elevation: 0,
             currentIndex: _selected,
             backgroundColor: Theme.of(context).colorScheme.onError,
-            onTap: (value) => setState(() => _selected = value),
+            onTap: (value) => setState(()=> _selected = value),
             type: BottomNavigationBarType.fixed,
             showSelectedLabels: false,
             selectedFontSize: 0,
             showUnselectedLabels: false,
             useLegacyColorScheme: false,
-            items: List.generate(
-                3,
-                (index) => BottomNavigationBarItem(
-                    activeIcon: SizedBox(
-                      height: 61,
-                      child: Column(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(
-                              bottom: 7,
-                            ),
-                            width: 65,
-                            height: 3,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          SvgPicture.asset(
-                            'assets/icon/${icons[index]}',
-                            color: Theme.of(context).primaryColor,
-                            width: 24,
-                            height: 24,
-                          ),
-                          const SizedBox(
-                            height: 4,
-                          ),
-                          Text(
-                            titles[index],
-                            style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 12, color: Theme.of(context).primaryColor),
-                          )
-                        ],
+            items:  List.generate(3, (index) => 
+              BottomNavigationBarItem(
+                activeIcon: SizedBox(
+                  height: 61,
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 7, ),
+                        width: 65,
+                        height: 3,
+                        color: Theme.of(context).primaryColor,
                       ),
-                    ),
-                    label: '',
-                    backgroundColor: Colors.white,
-                    icon: Column(
-                      children: [
-                        SvgPicture.asset(
-                          'assets/icon/${icons[index]}',
-                          width: 24,
-                          height: 24,
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          titles[index],
-                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 12, color: const Color(0xff959595)),
-                        )
-                      ],
-                    ))),
+                      SvgPicture.asset('assets/icon/${icons[index]}', color: Theme.of(context).primaryColor,width: 24, height: 24,),
+                      const SizedBox(height: 4,),
+                      Text(titles[index], style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 12,color: Theme.of(context).primaryColor),)
+                    ],
+                  ),
+                ),
+                label: '',
+                backgroundColor: Colors.white,
+                icon: Column(
+                  children: [
+                    SvgPicture.asset('assets/icon/${icons[index]}', width: 24, height: 24,),
+                    const SizedBox(height: 4),
+                    Text(titles[index], style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 12,color: const Color(0xff959595)),)
+                  ],
+                ))),
           ),
         ),
       ),
