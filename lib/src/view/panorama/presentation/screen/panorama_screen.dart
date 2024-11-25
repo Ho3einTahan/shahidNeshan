386a0f300e0482360194591750e2fc2e8bd3c9b4
ho3eintahan <hoseintahan84@gmail.com>
2024-11-25 23:22:20 +0330

    Update    panorama_screen.dart

---
+++ lib/src/view/panorama/presentation/screen/panorama_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shahid_neshan/src/core/constans/sizes.dart';
+import 'package:shahid_neshan/src/core/widget/custom_appbar_widget.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PanoramaScreen extends StatefulWidget {
+  const PanoramaScreen({super.key});
+
+  @override
+  State<PanoramaScreen> createState() => _PanoramaScreenState();
+}
+class _PanoramaScreenState extends State<PanoramaScreen> {
+  late WebViewController webViewController;
+  
+  @override
+  void initState() {
+    super.initState();
+    webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+        ),
+      )
+      ..loadRequest(Uri.parse('https://360.kimiasport.com/golzar/'));
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: CustomAppbarWidget(title: 'نمای ۳۶۰درجه گلزار شهدای کاشان', action: [], leadingWidth: Sizes.width(context)*0.8,fontSize: 16,),
+        body: WebViewWidget(controller: webViewController),
+      ),
+    );
+  }
+
+}
